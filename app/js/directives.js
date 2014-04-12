ffe22cf7634d038d048bca9306799078a844e38e
Niall Ryan <ryann4@tcd.ie>
2014-04-12 12:19:56 +0100

    Commenting on Directives and Filters

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,6 +2,9 @@
 
 /* Directives */
 
+// We'll have to write Directives if we want to include any jQuery plugins but I think they're best left to the end
+// We can write most of the code in Controllers first and then refactor into Directives and Services if time allows
+// Would like to use some though, they are cool.
 
 angular.module('bottleRocket.directives', []).
   directive('appVersion', ['version', function(version) {
--- app/js/filters.js
+++ app/js/filters.js
@@ -2,6 +2,8 @@
 
 /* Filters */
 
+// We probably won't need to write any filters unless one of you wants to try it out
+
 angular.module('bottleRocket.filters', []).
   filter('interpolate', ['version', function(version) {
     return function(text) {
