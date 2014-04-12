04f6553d2a9a5b47e619c392dc9e487e9cd4670f
Niall Ryan <ryann4@tcd.ie>
2014-04-12 12:15:23 +0100

    Adding thoughts on what services will need to be created

--- app/js/services.js
+++ app/js/services.js
@@ -7,3 +7,9 @@
 // In this case it is a simple value service.
 angular.module('bottleRocket.services', []).
   value('version', '0.1');
+
+// Most services we will need are just $http calls
+// For the Home and Artist pages, we'll need a service that makes a $http request to the Seevl or MusicBrainz APIs
+// The Artist page will need a service that makes a call to the YouTube and Soundcloud APIs
+// The Events page will need a service that makes a call to the BandsInTown APIs
+// We'll also need an auth service that calls the Facebook Auth API
