2a5094258ee47d3f6ed7f2820cb96ab57f90e982
Niall Ryan <ryann4@tcd.ie>
2014-04-14 12:32:37 +0100

    Adding comments to Services.js

--- app/js/services.js
+++ app/js/services.js
@@ -8,14 +8,44 @@
 angular.module('bottleRocket.services', []).
   value('version', '0.1')
 
-// Most services we will need are just $http calls
-// For the Home and Artist pages, we'll need a service that makes a $http request to the Seevl or MusicBrainz APIs
+// Most services we will need are just going to make $http calls
+// For the Home and Artist pages, we'll need a service that makes a $http request to the EchoNest, Seevl or MusicBrainz APIs
 // The Artist page will need a service that makes a call to the YouTube and Soundcloud APIs
 // The Events page will need a service that makes a call to the BandsInTown APIs
 // We'll also need an auth service that calls the Facebook Auth API
 
-	// basic factory service for getting data from Seevl API
+// &callback=JSON_CALLBACK must be appended to all URIs
+
+	.factory('echonestService', function() {
+		// this needs an API key which I do not want to check into Source Control,
+		// I'll come up with a workaround if we need to use it
+	})
+
 	.factory('seevlService', function($http) {
-		// just testing that a call to Seevl for data about "social+d" works
-		return $http.jsonp("http://data.seevl.fm/entities/?prefLabel=social+d&callback=JSON_CALLBACK");
+		// sample URL: http://data.seevl.fm/entities/?prefLabel=nirvana
+	})
+
+
+	.factory('musicbrainzService', function($http) {
+		// search API only returns XML I think so we might not use this
+	})
+
+	.factory('youtubeService', function($http) {
+		// this also needs an API key
+		// will be more complicated than the others so we'll leave it for a while
+	})
+
+	
+	.factory('soundcloudService', function($http) {
+		// this service might not be necessary, SC uses an SDK that we'll have to integrate, it's easy to implement but might take too much effort to refactor into a service
+	})
+
+	.factory('bandsintownService', function($http) {
+	//	The Bandsintown V2 API currently does not require authentication but does require that an application ID (app_id) parameter be passed with every request to identify yourself. See the example below:
+	//	http://api.bandsintown.com/artists/Skrillex/events.json?api_version=2.0&app_id=YOUR_APP_ID
+	//	The application ID can be anything, but should be a word that describes your application or company.
+
+	//	Sample URL for this request: http://api.bandsintown.com/artists/Skrillex.json?api_version=2.0&app_id=YOUR_APP_ID
+
+
 	});
